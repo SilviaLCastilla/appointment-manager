276e2ba3a1f77c1582d2418e31ee2bbff7333cc3
Tropical23 <silope0302@gmail.com>
2020-04-03 13:31:55 +0200

    proyecto maquetado

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,118 +1,121 @@
-import React, {Fragment, useState} from 'react';
-import uuid from 'uuid/v4';
-const Formulario = ({crearCita}) => {
-    //Crear State de Citas
-    const [cita, actualizarCita] = useState ({
-        mascota: '',
-        propietario: '',
-        fecha: '',
-        hora: '',
-        sintomas: ''
+import React, { Fragment, useState } from "react";
+import uuid from "uuid/v4";
+const Formulario = ({ crearCita }) => {
+  //Crear State de Citas
+  const [cita, actualizarCita] = useState({
+    mascota: "",
+    propietario: "",
+    fecha: "",
+    hora: "",
+    sintomas: ""
+  });
+
+  const [error, actualizarError] = useState(false);
+
+  //Función que se ejecuta para que el usario escriba un input
+
+  const actualizarState = e => {
+    actualizarCita({
+      ...cita /*lo hace para que coja todo el objeto, no solo una parte*/,
+      [e.target.name]: e.target.value
     });
+  };
 
-    const [error, actualizarError] = useState(false)
+  //Extraer los valores
+  const { mascota, propietario, fecha, hora, sintomas } = cita;
 
-    //Función que se ejecuta para que el usario escriba un input
+  //Cuando el usuario preciona agregar cita
+  const submitCita = e => {
+    e.preventDefault();
 
-    const actualizarState = e => {
-        actualizarCita({
-            ...cita, /*lo hace para que coja todo el objeto, no solo una parte*/
-            [e.target.name]: e.target.value
-        }
-        )
+    //Validar. Usamos trim para eliminar los espacios en blanco.
+    if (
+      mascota.trim() === "" ||
+      propietario.trim() === "" ||
+      fecha.trim() === "" ||
+      hora.trim() === "" ||
+      sintomas.trim() === ""
+    ) {
+      actualizarError(true);
+      return;
     }
 
-    //Extraer los valores
-    const {mascota,propietario,fecha,hora,sintomas} = cita;
-    
-    //Cuando el usuario preciona agregar cita
-    const submitCita = e => {
-        e.preventDefault();
+    //Eliminar mensaje previo
+    actualizarError(false);
+    //Asignar un ID
+    cita.id = uuid();
+    console.log(cita);
+    //Crear la cita
+    crearCita(cita);
+    //Reiniciar el form
+    actualizarCita({
+      mascota: "",
+      propietario: "",
+      fecha: "",
+      hora: "",
+      sintomas: ""
+    });
+  };
 
-        //Validar. Usamos trim para eliminar los espacios en blanco.
-        if(mascota.trim() === '' || propietario.trim() === '' || fecha.trim() === '' || hora.trim() === '' || sintomas.trim() === ''){
-            actualizarError(true)
-            return
-        }
+  return (
+    <Fragment>
+      <h2>Crear cita</h2>
+      {error ? (
+        <p className="alerta-error">Todos los campos son obligatorios</p>
+      ) : null}
+      <form onSubmit={submitCita}>
+        <label>Nombre Mascota</label>
+        <input
+          type="text"
+          name="mascota"
+          className="u-full-width"
+          placeholder="Nombre Mascota"
+          onChange={actualizarState}
+          value={mascota}
+        />
 
-        //Eliminar mensaje previo
-        actualizarError(false);
-        //Asignar un ID
-        cita.id = uuid();
-        console.log(cita);
-        //Crear la cita
-        crearCita(cita);
-        //Reiniciar el form
-        actualizarCita({
-            mascota: '',
-            propietario: '',
-            fecha: '',
-            hora: '',
-            sintomas: ''
-        })
-    }
+        <label>Nombre Dueño</label>
+        <input
+          type="text"
+          name="propietario"
+          className="u-full-width"
+          placeholder="Nombre dueño de la mascota"
+          onChange={actualizarState}
+          value={propietario}
+        />
 
-    return (
-        <Fragment>
-            <h2>Crear cita</h2>
-            {error ? <p className="alerta-error">Todos los campos son obligatorios</p> :null}
-            <form
-            onSubmit={submitCita}
-            >
-                <label>Nombre Mascota</label>
-                <input 
-                    type="text"
-                    name="mascota"
-                    className="u-full-width"
-                    placeholder="Nombre Mascota"
-                    onChange={actualizarState}
-                    value={mascota}
-                />
+        <label>Fecha</label>
+        <input
+          type="date"
+          name="fecha"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={fecha}
+        />
 
-                <label>Nombre Dueño</label>
-                <input 
-                    type="text"
-                    name="propietario"
-                    className="u-full-width"
-                    placeholder="Nombre dueño de la mascota"
-                    onChange={actualizarState}
-                    value={propietario}
-                />
+        <label>Hora</label>
+        <input
+          type="time"
+          name="hora"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={hora}
+        />
 
-                <label>Fecha</label>
-                <input 
-                    type="date"
-                    name="fecha"
-                    className="u-full-width"
-                    onChange={actualizarState}
-                    value={fecha}
-                />
+        <label>Sintomas</label>
+        <textarea
+          className="u-full-width"
+          name="sintomas"
+          onChange={actualizarState}
+          value={sintomas}
+        ></textarea>
 
-                <label>Hora</label>
-                <input 
-                    type="time"
-                    name="hora"
-                    className="u-full-width"
-                    onChange={actualizarState}
-                    value={hora}
-                />
+        <button type="submit" className="u-full-width button-primary">
+          Agregar cita
+        </button>
+      </form>
+    </Fragment>
+  );
+};
 
-                <label>Sintomas</label>
-                <textarea
-                    className="u-full-width"
-                    name="sintomas"
-                    onChange={actualizarState}
-                    value={sintomas}
-                ></textarea>
-
-                <button
-                type="submit"
-                className="u-full-width button-primary">
-                    Agregar cita
-                </button>
-            </form>
-        </Fragment>
-      );
-}
- 
 export default Formulario;
